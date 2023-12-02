e7b8ce3a668f9a08596c13b2e6bd01015ae9385f
jkim00844 <jkim00844@gmail.com>
2023-12-02 17:07:02 +0900

    first post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "tags": [],
+        "url": "/welcome/",
+        "teaser": null
+      }]
