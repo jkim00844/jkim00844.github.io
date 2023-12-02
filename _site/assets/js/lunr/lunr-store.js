2e1bb0c6522488cd5cd83db89ef4892e3ed4c418
jkim00844 <jkim00844@gmail.com>
2023-12-02 18:04:56 +0900

    update config

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Welcome to Jekyll!",
-        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
-        "tags": [],
-        "url": "/welcome/",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": ["welcome"],
+        "tags": ["hello"],
+        "url": "/welcome/welcome/",
         "teaser": null
       }]
